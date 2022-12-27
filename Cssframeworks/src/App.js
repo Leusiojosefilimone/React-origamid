3002a8ce25137b1b80e8b4b9397a8ea2f77a8361
leusiojosefilimone <leusiojosefilimone@gmail.com>
2022-12-27 13:39:58 -0300

    inserindo o bootstrap  no React

---
+++ Cssframeworks/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App( ) {
+  return (
+   
+    <div className='card bg-dark text-white m-5 ' style={{ maxwidth:'18rem', color: 'red'}}>
+      <div className='card-header'>Notebook</div>
+      <div className='card-body'>
+        <h5 className='card-title'>R$ 2500</h5>
+        <p className='card-text'>esse é  um notebook c0m 1tb, 16gb de ram e placa de video de 16gb</p>
+      </div>
+    </div>
+   
+  )
+
+  
+}
+
+export default App;
