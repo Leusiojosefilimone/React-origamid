e19f6f96aa229c39f75bf72c3cd24deee82ee35a
leusiojosefilimone <leusiojosefilimone@gmail.com>
2022-12-28 06:51:38 -0300

    animacoes

---
+++ Animacoes/src/Produto.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Produto = () => {
+  return (
+   <div className='animeLeft'>
+        <h2>Smartphone</h2>
+        <p>Preco: 1000mt</p>
+        <p>esse é um smartphone com 64gb de armazenamento e 3gb de ram</p>
+   </div>
+  )
+}
+
+export default Produto
